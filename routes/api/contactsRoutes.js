798c993077bd58f6dd3975718546628cd20c9be3
Tavsanka <domnikan@gmail.com>
2024-08-21 22:58:44 +0200

    update

---
+++ routes/api/contactsRoutes.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const contacts = require("../../models/contacts");
+const validateContact = require("../../middlewares/validateContact");
+const router = express.Router();
+
+router.get("/", async (req, res, next) => {
+  try {
+    const allContacts = await contacts.listContacts();
+    res.status(200).json(allContacts);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/:contactId", async (req, res, next) => {
+  try {
+    const contact = await contacts.getContactById(req.params.contactId);
+    if (!contact) {
+      return res.status(404).json({ message: "Not found" });
+    }
+    res.status(200).json(contact);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/", validateContact, async (req, res, next) => {
+  try {
+    const { name, email, phone } = req.body;
+    const newContact = await contacts.addContact({ name, email, phone });
+    res.status(201).json(newContact);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.put("/:contactId", validateContact, async (req, res, next) => {
+  try {
+    const updatedContact = await contacts.updateContact(
+      req.params.contactId,
+      req.body
+    );
+    if (!updatedContact) {
+      return res.status(404).json({ message: "Not found" });
+    }
+    res.status(200).json(updatedContact);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete("/:contactId", async (req, res, next) => {
+  try {
+    const contact = await contacts.removeContact(req.params.contactId);
+    if (!contact) {
+      return res.status(404).json({ message: "Not found" });
+    }
+    res.status(200).json({ message: "Contact deleted" });
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
